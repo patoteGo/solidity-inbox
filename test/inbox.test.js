29b2b9c1da0aa9183d6306d823b952acb74292c9
Patricio Gonzalez <patote.gonzalwz@gmail.com>
2021-10-19 21:45:48 -0400

    test with getAccounts with ganesh

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,27 +3,15 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-class Car {
-	park() {
-		return 'stopped';
-	}
-
-	drive() {
-		return 'vroom';
-	}
-}
-
-let car;
 beforeEach(() => {
-	car = new Car();
+	//Get a List of all accounts
+	web3.eth.getAccounts().then((fetchedAccounts) => {
+		console.log(fetchedAccounts);
+	});
+	//Use one of those accounts to deploy
+	//the contract
 });
 
-describe('Car', () => {
-	it('can park', () => {
-		assert.equal(car.park(), 'stopped');
-	});
-
-	it('can drive', () => {
-		assert.equal(car.drive(), 'vroom');
-	});
+describe('Inbox', () => {
+	it('deploys a contract', () => {});
 });
